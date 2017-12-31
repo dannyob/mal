9fb9f4dd2f7ad98f5453e0755a5133619f3ad6e1
Danny O'Brien <danny@spesh.com>
2017-12-30 19:05:36 -0800

    early tokenizer

--- haskell2/Reader.hs
+++ haskell2/Reader.hs
@@ -1,5 +1,10 @@
 module Reader () where
 
-foo = undefined
+import Text.Regex.PCRE.String as TR
 
+tokenregexp = '[\s,]*(~@|[\[\]{}()'`~^@]|"(?:\\.|[^\\"])*"|;.*|[^\s\[\]{}('"`,;)]*)'
+compiled_token = TR.compile (TR.CompOption 0) (TR.ExecOption 0) tokenregexp
+
+tokenizer :: String -> [String]
+tokenizer aline tokens = ["foo", "bar")
 
