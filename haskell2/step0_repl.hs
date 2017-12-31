b5cb0ef04d96ba8a1e23325d4a958e18707789f6
Danny O'Brien <danny@spesh.com>
2017-12-30 18:36:39 -0800

    Initial REPL implementation

--- haskell2/step0_repl.hs
+++ haskell2/step0_repl.hs
@@ -1,3 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
 
+import System.IO
 
-main = undefined
+malREAD :: String -> String
+malREAD x = x
+
+malEVAL :: String -> String
+malEVAL x = x
+
+malPRINT :: String -> String
+malPRINT x = x
+
+rep :: String -> String
+rep x = malPRINT $ malEVAL $ malREAD x
+
+repl = do
+    input <- getLine
+    putStrLn $ rep input
+    hFlush stdout
+    repl
+
+main = repl
