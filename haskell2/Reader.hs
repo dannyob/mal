284d56dab8b4f53018059a2c00295e84f8945ddb
Seth Schoen <schoen@eff.org>
2017-12-30 19:09:52 -0800

    Escape the tokenizer regexp

--- haskell2/Reader.hs
+++ haskell2/Reader.hs
@@ -2,9 +2,9 @@ module Reader () where
 
 import Text.Regex.PCRE.String as TR
 
-tokenregexp = '[\s,]*(~@|[\[\]{}()'`~^@]|"(?:\\.|[^\\"])*"|;.*|[^\s\[\]{}('"`,;)]*)'
+tokenregexp = "[\\s,]*(~@|[\\[\\]{}()'`~^@]|\"(?:\\\\.|[^\\\\\"])*\"|;.*|[^\\s\\[\\]{}('\"`,;)]*)"
+
 compiled_token = TR.compile (TR.CompOption 0) (TR.ExecOption 0) tokenregexp
 
 tokenizer :: String -> [String]
-tokenizer aline tokens = ["foo", "bar")
-
+tokenizer aline = ["foo", "bar"]
