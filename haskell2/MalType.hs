7004f0d51f28dd5df3e2820a854a91022a2af1c3
Seth Schoen <schoen@eff.org>
2018-01-29 18:25:35 -0800

    Derive Eq to make guard agree that [] == []

--- haskell2/MalType.hs
+++ haskell2/MalType.hs
@@ -1,5 +1,3 @@
 module MalType (MalType(MalString, MalNumber, MalList, MalVector, MalSymbol, MalNil, MalTrue, MalFalse)) where
 
-data MalType = MalString String | MalNumber Int | MalList [MalType] | MalVector [MalType] | MalSymbol String | MalNil | MalTrue | MalFalse deriving Show
-
-
+data MalType = MalString String | MalNumber Int | MalList [MalType] | MalVector [MalType] | MalSymbol String | MalNil | MalTrue | MalFalse deriving (Show, Eq)
