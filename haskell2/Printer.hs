a09a2340e8cc2103f921fd48a9709cf0d6a3573b
Seth Schoen <schoen@eff.org>
2018-01-29 20:36:04 -0800

    Support for unescaping strings

--- haskell2/Printer.hs
+++ haskell2/Printer.hs
@@ -2,6 +2,13 @@ module Printer (pr_str) where
 
 import MalType
 
+unescape "" = ""
+unescape (x:xs)
+    | x == '\n' = "\\n" ++ unescape xs
+    | x == '\\' = "\\\\" ++ unescape xs
+    | x == '"' = "\\\"" ++ unescape xs
+    | otherwise = x:(unescape xs)
+
 pr_list :: [MalType] -> String
 pr_list (x:xs)
     | xs == [] = pr_str x
@@ -13,9 +20,6 @@ pr_str MalTrue = "true"
 pr_str MalFalse = "false"
 pr_str (MalNumber n) = show n
 pr_str (MalSymbol s) = s
-pr_str (MalString s) = "\"" ++ s ++ "\""    -- XXX: This is not correct,
-                                            --      because of escaping.
-                                            --      (We still need to add \
-                                            --      before special characters.)
+pr_str (MalString s) = "\"" ++ (unescape s) ++ "\""
 pr_str (MalList x) = "(" ++ (pr_list x) ++ ")"
 pr_str (MalVector x) = "[" ++ (pr_list x) ++ "]"
