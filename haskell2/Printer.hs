7291df438e8afc48c2c8ed4cf1284f0465b01c02
Seth Schoen <schoen@eff.org>
2018-01-31 11:13:44 -0800

    Add print_readably option (turn escaping on/off)

--- haskell2/Printer.hs
+++ haskell2/Printer.hs
@@ -9,17 +9,20 @@ escape (x:xs)
     | x == '"' = "\\\"" ++ escape xs
     | otherwise = x:(escape xs)
 
-pr_list :: [MalType] -> String
-pr_list (x:xs)
-    | xs == [] = pr_str x
-    | otherwise = (pr_str x) ++ " " ++ (pr_list xs)
+pr_list :: [MalType] -> Bool -> String
+pr_list (x:xs) print_readably
+    | xs == [] = pr_str x print_readably
+    | otherwise = (pr_str x print_readably) ++ " " ++ (pr_list xs print_readably)
 
-pr_str :: MalType -> String
-pr_str MalNil = "nil"
-pr_str MalTrue = "true"
-pr_str MalFalse = "false"
-pr_str (MalNumber n) = show n
-pr_str (MalSymbol s) = s
-pr_str (MalString s) = "\"" ++ (escape s) ++ "\""
-pr_str (MalList x) = "(" ++ (pr_list x) ++ ")"
-pr_str (MalVector x) = "[" ++ (pr_list x) ++ "]"
+pr_str :: MalType -> Bool -> String
+pr_str MalNil _ = "nil"
+pr_str MalTrue _ = "true"
+pr_str MalFalse _ = "false"
+pr_str (MalNumber n) _ = show n
+pr_str (MalSymbol s) _ = s
+pr_str (MalString s) True = "\"" ++ (escape s) ++ "\""
+pr_str (MalString s) False = "\"" ++ s ++ "\""  -- I think maybe this isn't
+                                                -- supposed to have quotes
+                                                -- around it in this case?
+pr_str (MalList x) print_readably = "(" ++ (pr_list x print_readably) ++ ")"
+pr_str (MalVector x) print_readably = "[" ++ (pr_list x print_readably) ++ "]"
