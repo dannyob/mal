112d8b18b5f9a3bc169a9f92bd7f5bc5a0dfc2b6
Seth Schoen <schoen@eff.org>
2018-03-11 18:45:20 -0700

    Beginning of step 3

---
+++ haskell2/step3_env.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import System.IO
+import Reader
+import Printer
+import MalType
+import Eval
+import System.Console.Readline
+import Data.Maybe
+
+-- import Data.Map
+
+malREAD :: [String] -> Either String MalType
+malREAD x = do
+        rf <- read_form x
+        return (fst $ rf)
+
+malPRINT :: MalType -> String
+malPRINT x = pr_str x True
+
+rep :: [String] -> String
+rep x = case malREAD x of
+        Left err -> err
+        Right mts -> malPRINT $ malEVAL repl_env mts
+
+repl = do
+    minput <- readline "user> "
+    addHistory $ fromMaybe "" minput
+    tokens <- tokenizer $ fromMaybe "" minput
+    if tokens /= [] then do
+        putStrLn $ rep tokens
+        hFlush stdout
+    else return ()
+    repl
+
+main = repl
