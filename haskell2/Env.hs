bcdf2e7a3d932b991ad49f79c4a987d3a82d69e7
Danny O'Brien <danny@spesh.com>
2018-03-25 18:46:17 -0700

    Export environment functions.
    I also changed their arguments to accept strings rather than MalTypes,
    as this was more consistent with what Mal expects.

--- haskell2/Env.hs
+++ haskell2/Env.hs
@@ -1,4 +1,4 @@
-module Env (MalEnv, mkMalEnv) where
+module Env (MalEnv, mkMalEnv, get, find, set) where
 import MalType
 import Data.Map
 import Data.Maybe
@@ -8,24 +8,24 @@ import Data.IntMap as DI
 data MalEnvEntry = MalEnvEntry { meOuter :: Maybe Int , meData :: Map String MalType } deriving Show
 data MalEnv = MalEnv { meBag :: DI.IntMap MalEnvEntry, meCurrent :: Int } deriving Show
 
-set :: MalEnv -> MalType -> MalType -> MalEnv
-set (MalEnv oldBag current) (MalSymbol k) v =
+set :: MalEnv -> String -> MalType -> MalEnv
+set (MalEnv oldBag current) k v =
     let updateEntry (MalEnvEntry outer dat) = MalEnvEntry outer (Data.Map.insert k v dat)
         updateBag = DI.adjust updateEntry current oldBag in
         MalEnv updateBag current
 
-find :: MalEnv -> MalType -> Maybe MalEnv
-find me@(MalEnv bag current) (MalSymbol k) =
+find :: MalEnv -> String -> Maybe MalEnv
+find me@(MalEnv bag current) k =
     if Data.Map.member k (meData $ bag DI.! current) then 
     Just me
     else
         do
             parent <- meOuter $ bag DI.! current
-            find (MalEnv bag parent) (MalSymbol k)
+            find (MalEnv bag parent) k
 
-get :: MalEnv -> MalType -> Maybe MalType
-get me@(MalEnv bag current) (MalSymbol k)  = do
-    f <- find me (MalSymbol k)
+get :: MalEnv -> String -> Maybe MalType
+get me@(MalEnv bag current) k  = do
+    f <- find me k
     Data.Map.lookup k (getCurrentData f)
 
 mkMalEnv :: [(String,MalType)] -> MalEnv
